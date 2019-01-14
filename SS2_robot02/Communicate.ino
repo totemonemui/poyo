afb06806c4671cff21a80ebc5c8694e9c8f16cae
Unknown <ss2.2018.7squad@gmail.com>
2019-01-14 11:57:12 +0900

    通信追加

---
+++ SS2_robot02/Communicate.ino
@@ -0,0 +1,15 @@
+
+//無線機器を通じて相方の機体と通信する
+//esp,xbeeどちらでも大丈夫(の予定)
+
+int sendMyState(int state){
+    if(state >= 0 && state <= 0xFF){
+        mySerial.write(state);
+        return 0;
+    }
+    else return -1;
+}
+
+int getPartnerState(void){
+    return (int)(mySerial.peak());
+}
