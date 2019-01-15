854bfd2812782c2333219159320004d55d3f057c
NodaRyohei <ryoheinod@gmail.com>
2019-01-15 10:32:34 +0900

    通信関係

--- SS2_Robot_Main/Communicate.ino
+++ SS2_Robot_Main/Communicate.ino
@@ -2,16 +2,16 @@
 //無線機器を通じて相方の機体と通信する
 //esp,xbeeどちらでも大丈夫(の予定)
 
-int sendMyState(int state){
-    if(state >= 0 && state <= 0xFF){
-        mySerial.write(state);
-        return 0;
-    }
-    else return -1;
+int sendMyState(int state) {
+  if (state >= 0 && state <= 0xFF) {
+    mySerial.write(state);
+    return 0;
+  }
+  else return -1;
 }
 
-int getPartnerState(void){
-    static char data = 0xFF;
-    while(mySerial.available())data = mySerial.read();//最後に送られた文字のみ保存する
-    return (int)data;
+int getPartnerState(void) {
+  static char data = 0xFF;
+  while (mySerial.available())data = mySerial.read(); //最後に送られた文字のみ保存する
+  return (int)data;
 }
